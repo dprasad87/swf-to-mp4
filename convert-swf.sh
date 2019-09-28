4b8e502a96a11911fda33ac7508d32356cdafd76
Dwarika Prasad <dwarika.prasad87@gmail.com>
2019-09-28 17:06:24 +0530

    Processing directory

--- convert-swf.sh
+++ convert-swf.sh
@@ -3,9 +3,5 @@
 FILENAME=$1
 cd /data
 OWNER=`ls -ld ${FILENAME} | awk '{print $3}'`
-echo "OWNER : "+ ${OWNER}
-cp /data/swf_to_mp4.sh /
-rm /data/swf_to_mp4.sh
-chmod u+x /swf_to_mp4.sh
 /swf_to_mp4.sh $FILENAME
 chown $OWNER ${FILENAME%.*}.mp4
